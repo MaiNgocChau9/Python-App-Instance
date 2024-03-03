90dce0a3ac84a5883d1847e1cd3b9ff94f10f763
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-03 14:56:43 +0700

    commit

---
+++ Buổi 1/b1.py
@@ -0,0 +1,11 @@
+class ghe:
+    def __init__(self, chan_ghe, cho_ngoi, ghe_tua):
+        self.chan_ghe = chan_ghe
+        self.cho_ngoi = cho_ngoi
+        self.ghe_tua = ghe_tua
+    def XuatThongTin(self):
+        print("Chan ghe:", self.chan_ghe)
+        print("Cho ngoi:", self.cho_ngoi)
+        print("Ghe tua:", self.ghe_tua)
+ghe = ghe("gỗ", "gỗ", "nhựa")
+ghe.XuatThongTin()
