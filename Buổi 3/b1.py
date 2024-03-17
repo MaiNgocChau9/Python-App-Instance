c54aea47405472ca535b97cdc6c9e23d2cb24eb9
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-17 14:57:30 +0700

    commit

---
+++ Buổi 3/b1.py
@@ -0,0 +1,18 @@
+class Object():
+    def __init__(self, id, img, title, date, rating):
+        self.id = id
+        self.img = img
+        self.title = title
+        self.date = date
+        self.rating = rating
+    
+    def print(self):
+        print(self.id)
+        print(self.img)
+        print(self.title)
+        print(self.date)
+        print(self.rating)
+
+
+a = Object(input('id: '), input('img: '), input('title: '), input('date: '), input('rating: '))
+a.print()
