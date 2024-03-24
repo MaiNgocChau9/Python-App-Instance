6a74fff55372326631cfb373bba966089c5072ae
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-24 15:10:44 +0700

    commit

---
+++ Buổi 4/b1.py
@@ -0,0 +1,13 @@
+def def_max(a, b, c): 
+    
+    max_num = a
+    if b > max_num: max_num = b
+    if c > max_num: max_num = c
+
+    return max_num
+
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+print(def_max(a, b, c))
