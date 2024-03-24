5d08eb23dce349ff9becfbe0b49503c0b01754e0
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-24 16:05:29 +0700

    commit

---
+++ Buổi 4/Bài 2.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self):
+        
+        self.name = "Chau"
+        self.age = 12
+        self.grade = 7
+        # self.danh_sach = list()
+
+print(Student.grade)
