f5da384afcc6848719a57f227e020385f83d6351
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-24 15:14:03 +0700

    commit

--- Buổi 4/Bài 1.py
+++ Buổi 4/Bài 1.py
@@ -11,3 +11,15 @@ b = float(input("b: "))
 c = float(input("c: "))
 
 print(def_max(a, b, c))
+
+
+""" 
+CÁCH KHÁC :)))
+
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+print(max(a, b, c))
+
+"""
