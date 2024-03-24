c0efc03b89d33329beca7590dffde80493f10d74
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-24 16:16:27 +0700

    commit

--- Buổi 4/Bài 2.py
+++ Buổi 4/Bài 2.py
@@ -1,8 +1,31 @@
 class Student:
-    def __init__(self, name, age, grade, danh_sach):
+    def __init__(self, name, age, grade):
         
         self.name = name
         self.age = age
         self.grade = grade
-        self.danh_sach = danh_sach
+        
+student_list = []
 
+# Add students to student_list
+def add_student(name, age, grade):
+    student_list.append(Student(name, age, grade))
+
+# Remove students from student_list
+def remove_student(name):
+    delete_student = input("Enter the name of the student you want to delete: ")
+    for student in student_list:
+        if student.name == delete_student:
+            student_list.remove(student)
+            print("Deleted successfully")
+            break
+        else:
+            print("Not found")
+            break
+
+# Display students in student_list
+def display_student():
+    for student in student_list:
+        print(student.name, student.age, student.grade)
+
+# Update students in student_list
