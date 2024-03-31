13e17c3e3f7ec997702dfe38b1193b64e4ab9405
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-03-31 15:45:01 +0700

    commit

--- Buổi 5/bai1.py
+++ Buổi 5/bai1.py
@@ -22,23 +22,56 @@ class HomeworkList:
 # Tạo danh sách các bài tập
 homeworklist = HomeworkList()
 
-# Thêm các bài tập vào danh sách
-homeworklist.add(Homework("Lập trình App Producer", 3, True))
-homeworklist.add(Homework("Làm văn", 2, False))
-homeworklist.add(Homework("Lập trình Gamemaker", 1, False))
+# # Thêm các bài tập vào danh sách
+# homeworklist.add(Homework("Lập trình App Producer", 3, True))
+# homeworklist.add(Homework("Làm văn", 2, False))
+# homeworklist.add(Homework("Lập trình Gamemaker", 1, False))
 
-# Kiểm tra
-homeworklist.all_finished()
+# # Kiểm tra
+# homeworklist.all_finished()
 
-for item in homeworklist.un_finished:
-    print("Tên:", item.name)
+# for item in homeworklist.un_finished:
+#     print("Tên:", item.name)
 
-    print("Mức độ quan trọng: ", end="")
-    if item.priority == 3: print("Cao")
-    elif item.priority == 2: print("Trung bình")
-    elif item.priority == 1: print("Thấp")
-    else: print("Không xác định")
+#     print("Mức độ quan trọng: ", end="")
+#     if item.priority == 3: print("Cao")
+#     elif item.priority == 2: print("Trung bình")
+#     elif item.priority == 1: print("Thấp")
+#     else: print("Không xác định")
 
-    if item.completed == True: print("Đã hoàn thành")
-    else: print("Chưa hoàn thành")
-    print()
+#     if item.completed == True: print("Đã hoàn thành")
+#     else: print("Chưa hoàn thành")
+#     print()
+
+# Main program
+while True:
+    print("PHẦN MỀM QUẢN LÍ BÀI TẬP")
+    print("1. Thêm bài tập")
+    print("2. Xem bài tập chưa hoàn thành")
+    print("3. Thoát")
+    choice = int(input("\nChọn chức năng: "))
+
+    if choice == 1:
+        while True:
+            name = input("\nTên: ")
+            if name == "": break
+            priority = int(input("Mức độ quan trọng ( 1: Thấp | 2: Trung bình | 3: Cao ): "))
+            completed = input("Đã hoàn thành (y/n): ")
+            if completed == "y": completed = True
+            else: completed = False
+            homeworklist.add(Homework(name, priority, completed))
+    elif choice == 2:
+        print("\nNHỮNG BÀI TẬP CHƯA HOÀN THÀNH")
+        homeworklist.all_finished()
+
+        for item in homeworklist.un_finished:
+            print("Tên:", item.name)
+
+            print("Mức độ quan trọng: ", end="")
+            if item.priority == 3: print("Cao")
+            elif item.priority == 2: print("Trung bình")
+            elif item.priority == 1: print("Thấp")
+            else: print("Không xác định")
+            print()
+    
+    else: break
