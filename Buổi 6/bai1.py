8d35de07bcaed09ba52735777bfb4c03a98a7eaa
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-07 15:04:18 +0700

    commit

--- Buổi 6/bai1.py
+++ Buổi 6/bai1.py
@@ -16,9 +16,10 @@ print(data)
 # Đọc với ghi file
 new_data = {"name": "abc", "title": "abc", "date": "abc", "image": "abc", "rating": "abc"}
 with open("Buổi 6\data.json", "w") as f:
-    json.dump(new_data, f)
+    json.dump(new_data, f, indent=4)
+    # Không có indent=4 cũng được, nó chỉ giúp file JSON đẹp hơn thôi :V
 
 # Đọc và in (để xem kết quả mới nhất)
 with open ('Buổi 6\data.json', "r") as f:
     data = json.load(f)
-print(data)
+print("\nNew data:",data)
