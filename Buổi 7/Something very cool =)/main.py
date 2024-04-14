bc327be8b496b0b14ce900f265a46ac410dbfaae
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-14 15:39:11 +0700

    commit

--- Buổi 7/Something very cool =)/main.py
+++ Buổi 7/Something very cool =)/main.py
@@ -9,7 +9,18 @@ class Main(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("Buổi 7\\Something very cool =)\\breh.ui", self)
-        self.show()
+        a = ["item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"]
+        self.listWidget_2.addItems(a)
+        self.pushButton_11.clicked.connect(self.add)
+        self.pushButton.clicked.connect(self.remove)
+        print(self.listWidget_2.count())
+        
+    def add(self):
+        self.listWidget_2.addItem(self.lineEdit.text())
+        self.lineEdit.clear()
+    
+    def remove(self):
+        self.listWidget_2.takeItem(self.listWidget_2.currentRow())
 
 
 app = QApplication(sys.argv)
